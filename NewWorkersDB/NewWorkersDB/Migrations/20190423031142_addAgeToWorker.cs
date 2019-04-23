8c72fcb9b2057c88e7c7c6065471ca2e51385dd1
Марина Шустова <mshustovam@gmail.com>
2019-04-23 10:14:58 +0700

    Добавила поле Age работнику

---
+++ NewWorkersDB/NewWorkersDB/Migrations/20190423031142_addAgeToWorker.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewWorkersDB.Migrations
+{
+    public partial class addAgeToWorker : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Age",
+                table: "Worker",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Age",
+                table: "Worker");
+        }
+    }
+}
--- NewWorkersDB/NewWorkersDB/Models/Worker.cs
+++ NewWorkersDB/NewWorkersDB/Models/Worker.cs
@@ -7,6 +7,7 @@ namespace NewWorkersDB.Models {
     public class Worker {
         public int ID { get; set; }
         public string FullName { get; set; }
+        public int Age { get; set; }
 
         public virtual ICollection<Project> Projects { get; set; }
     }
